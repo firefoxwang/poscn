5cf7e2ed50cb01b99a918b33dd62857464538b19
firefoxwang <1228137800@qq.com>
2026-08-14 15:29:20 +0800

    miniprogram: point BASE_URL to http://192.168.31.8:4202/api

--- miniprogram/consumer/utils/config.js
+++ miniprogram/consumer/utils/config.js
@@ -1,6 +1,6 @@
 // 本地开发默认指向本机后端。
 // 微信开发者工具需在「详情 -> 本地设置」勾选「不校验合法域名」。
 // 生产环境请替换为已 ICP 备案的 HTTPS 域名，并加入小程序后台的合法 request 域名白名单。
-const BASE_URL = 'http://127.0.0.1:8020';
+const BASE_URL = 'http://192.168.31.8:4202/api';
 
 module.exports = { BASE_URL };
